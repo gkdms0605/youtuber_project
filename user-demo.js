8fde28e96928e555fec4ab2df1a301102feec103
gkdms0605 <leehaeun060523@gmail.com>
2025-02-24 14:22:10 +0900

    add user_inform & user_delete page with route

--- user-demo.js
+++ user-demo.js
@@ -19,17 +19,13 @@ db.set(1, user)
 
 // 회원 로그인 (post)
 app.post('/login', (req, res) => {
-    res.json({
-        message: `${name}님 환영합니다.`
-    })
+
 })
 
 // 회원 가입 (post)
 app.post('/join', (req, res) => {
     let insertData = req.body
-    if(insertData)
-
-    if(insertData) {
+    if(insertData != {}) {
         db.set(id, req.body)
         res.status(201).json({
             message: `${db.get(id++).name}님 환영합니다.`
@@ -43,23 +39,42 @@ app.post('/join', (req, res) => {
     
 }) 
 
-// 회원 개별 조회 (get)
-app.get('/users/:id', (req, res) => {
-    let {id} = req.params
-    id = parseInt(id)
-    
-    res.json({
-        id: res.id,
-        name: res.name
-    })
-})
+app
+    .route('/users/:id')
 
-// 회원 탈퇴 (delete)
-app.delete('/users/:id', (req, res) => {
-    let {id} = req.params
-    id = parseInt(id)
-    
-    res.json({
-        message: `${name}님 다음에 또 뵙겠습니다.`
+    // 회원 개별 조회 (get)
+    .get((req, res) => {
+        let {id} = req.params
+        id = parseInt(id)
+        let userData = db.get(id)
+        
+        if(userData) {
+            res.status(200).json({
+                userId: userData.userId,
+                name: userData.name
+            })
+        } else {
+            res.status(404).json({
+                message: "회원 정보가 없습니다."
+            })
+        }
     })
-})
+
+    // 회원 탈퇴 (delete)
+    .delete((req, res) => {
+        let {id} = req.params
+        id = parseInt(id)
+        let userData = db.get(id)
+        
+        if(userData) {
+            let userName = userData.name
+            db.delete(id)        
+            res.status(200).json({
+                message: `${userName}님, 다음에 또 뵙겠습니다.`
+            })
+        } else {
+            res.status(404).json({
+                message: "회원 정보가 없습니다."
+            })
+        }
+    })
