ac2dcf4148c47e2189c955d54c812edb546e65d5
gkdms0605 <leehaeun060523@gmail.com>
2025-03-05 01:59:24 +0900

    new module: express-validator

--- routes/channels.js
+++ routes/channels.js
@@ -1,8 +1,11 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 const conn = require('../mariadb')
+const {body, param, validationResult} = require('express-validator')
+
 router.use(express.json())
 
+// functions
 function notFoundId(res) {
     res.status(404).send("해당하는 ID의 채널 정보가 없습니다.")
 }
@@ -12,13 +15,27 @@ function stringToInt(req) {
     return parseInt(id)
 }
 
+const validate = (req, res) => {
+    const err = validationResult(req)
+
+    if(!err.isEmpty){
+        return res.status(400).json(err.array())
+    }
+}
+
 router
     .route("/")
-    .get((req, res) => {
-        let {user_id} = req.body
+    .get(
+        [
+            body('user_id').notEmpty().isInt().withMessage('숫자를 입력해주세요.'),
+            validate
+        ],
+        (req, res) => {
+            validate(req, res)
 
-        let sql = `SELECT * FROM channels WHERE id = ? `
-        if (user_id) {
+            let {user_id} = req.body
+
+            let sql = `SELECT * FROM channels WHERE user_id = ? `
             conn.query(sql, user_id,
                 (err, results) => {
                     if(results.length){
@@ -28,77 +45,122 @@ router
                     }
                 }
             )
-        } 
+    })
+    
+    .post(
+        [body('userId').notEmpty().isInt().withMessage('숫자를 입력해주세요.'),
+        body('name').notEmpty().isString().withMessage('문자를 입력해주세요.')],
+            (req, res) => {
+                const err = validationResult(req)
 
-        else {
-            res.status(400).end()
-        }
-    })
-    
-    .post((req, res) => {
-        const {name, userId} = req.body
-        
-        if(name && userId){
-            let sql = `INSERT INTO channels (name, user_id) VALUES (?, ?)`
-            conn.query(sql, [name, userId],
-                (err, results) => {
-                    res.status(201).json(results)
+                if(!err.isEmpty()) {
+                    return res.status(400).json(err.array())
                 }
-            )
-        } else {
-            res.status(400).send("요청값을 확인해 주세요!")
-        }
-    })
-    
+                const {name, userId} = req.body
+                
+                let sql = `INSERT INTO channels (name, user_id) VALUES (?, ?)`
+                conn.query(sql, [name, userId],
+                    (err, results) => {
+                        if(err) {
+                            console.log(err)
+                            return res.status(400).end()
+                        }
+                        res.status(201).json(results)
+                    }
+                )
+            }
+        )
 
 router
     .route("/:id")
-    .get((req, res) => {
+    .get( param().notEmpty().withMessage('채널 id 필요'),
+        (req, res) => {
+        const err = validationResult(req)
+
+        if(!err.isEmpty()) {
+            return res.status(400).json(arr.array())
+        }
+
         let id = stringToInt(req.params)
 
         let sql = `SELECT * FROM channels WHERE id = ? `
         conn.query(sql, id,
             (err, results) => {
+                if(err){
+                    console.log(err)
+                    res.status(400).end()
+                } 
+                
                 if(results.length){
                     res.status(200).json(results)
-                } else {
+                }
+                else {
                     notFoundId(res)
                 }
             }
         )
     })
 
-    .put((req, res) => {
+    .put([param('id').notEmpty().withMessage('채널 id 필요'),
+        body('name').notEmpty().isString().withMessage('채널명 오류'),
+    ],
+        (req, res) => {
+        const err = validationResult(req)
+
+        if(!err.isEmpty()) {
+            return res.status(400).json(err.array())
+        }
+
+        let {name} = req.body
         let {id} = req.params
         id = parseInt(id)
-        let channel = db.get(id)
 
-        if(channel) {
-            if(Object.keys(req.body).length){
-                let oldTitle = channel.channelTitle 
-                let newTitle = req.body.channelTitle
-                db.set(id, req.body)
+        let values = [name, id]
+        
+        let sql = 'UPDATE channels SET name = ? WHERE id = ?'
+        conn.query(sql, values,
+            (err, results) => {
+                if(err) {
+                    console.log(err)
+                    return res.status(400).end()
+                }
 
-                res.status(200).send(`채널명이 성공적으로 변경되었습니다. ${oldTitle} -> ${newTitle}`)
-            } else {
-                res.status(400).send("채널명을 입력해주세요.")
+                if(results.affectedRows == 0) {
+                    return notFoundId(res)
+                }
+                else {
+                    res.status(200).json(results)
+                }
             }
-        } else {
-            notFoundId()
-        }
+        )
     })
 
-    .delete((req, res) => {
+    .delete(param('id').notEmpty().isInt().withMessage('채널 ID 필요'),
+        (req, res) => {
+        const err = validationResult(req)
+
+        if(!err.isEmpty()){
+            return res.status.json(err.array())
+        }
         let {id} = req.params
         id = parseInt(id)
-        let channel = db.get(id)
+        
+        let sql = "DELETE FROM channels WHERE id = ?"
 
-        if(channel) {
-            db.delete(id)
-            res.status(200).send(`${channel.channelTitle}이 삭제 되었습니다.`)
-        } else {
-            notFoundId()
-        }
+        conn.query(sql, id, 
+            (err, results) => {
+                if(err){
+                    return res.status(400).json(err)
+                }
+
+                if(results.affectedRows == 0){
+                    return  res.status(400).end()
+                }
+                else {
+                    res.status(200).json(results)
+                }                
+            }
+        )
     })
 
 module.exports = router
